883a42b2d67db4e8475816dc8e858ae0cb0e4ffc
cdxii <104383966+idunwannagotoschool@users.noreply.github.com>
2026-07-25 19:49:21 +0800

    Update docusaurus.config.ts

--- webpage/docusaurus.config.ts
+++ webpage/docusaurus.config.ts
@@ -12,10 +12,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://neko.m1k1o.net',
+  url: 'https://idunwannagotoschool.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/neko/',
 
   // GitHub pages deployment config.
   organizationName: 'm1k1o',
